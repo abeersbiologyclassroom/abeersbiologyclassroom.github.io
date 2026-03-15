b27dad027418a641cbd410696dd6637c6370c0a8
Md Abeer Hussain <abeer.rumc@gmail.com>
2026-03-15 10:25:44 +0600

    Add files via upload

---
+++ exams/organic.js
@@ -0,0 +1,31 @@
+// Inside exams/test1.js
+EXAM_DATA = {
+  title: "Organic Chemistry",
+  durationMinutes: 10,
+  questions: [
+    {
+      id: 1,
+      text: "অ্যালকেনের সাধারণ সংকেত কোনটি?",
+      options: [
+        "$\\ce{C_{n}H_{2n+2}}$",
+        "$\\ce{C_{n}H_{2n}}$",
+        "$\\ce{C_{n}H_{n}}$",
+        "$\\ce{C_{n}H_{2n-2}}$",
+      ],
+      correctOptionIndex: 0,
+      explanation:
+        "অ্যালকেন হলো সম্পৃক্ত হাইড্রোকার্বন, যার কার্বন-কার্বন একক বন্ধন থাকে। এর সাধারণ সংকেত $\\ce{C_{n}H_{2n+2}}$।",
+    },
+    {
+      id: 2,
+      text: "What is ATP?",
+      options: [
+        "adenosine triphosphate",
+        "adenosine diphosphate",
+        "adenosine monophosphate",
+        "adenosine tetraphosphate",
+      ],
+      correctOptionIndex: 0,
+    },
+  ],
+};
